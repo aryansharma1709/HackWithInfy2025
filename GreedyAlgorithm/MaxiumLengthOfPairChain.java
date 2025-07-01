20823cbc1017f5a50089d496c09217e6447185ab
Aryan Sharma <aryansharmag29@gmail.com>
2025-07-01 09:37:44 +0530

    Greedy problems

---
+++ GreedyAlgorithm/MaxiumLengthOfPairChain.java
@@ -0,0 +1,33 @@
+ import java.util.*;
+public class MaxiumLengthOfPairChain {
+
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+         int  n=sc.nextInt();
+         int [][] arr=new int [n][2];
+         for(int i=0;i<n;i++)
+         {
+            arr[i][0]=sc.nextInt();
+            arr[i][1]=sc.nextInt();
+
+         }
+        System.out.println(personMeeeting(arr));
+    }
+    public static int personMeeeting(int [][]arr)
+    {
+         Arrays.sort(arr, (a,b)->Integer.compare(a[1],b[1]));
+        int cnt=1;
+        int max=arr[0][1];
+        for(int i=1;i<arr.length;i++)
+        {
+            if(max<arr[i][0])
+            {
+                cnt++;
+                max=arr[i][1];
+
+            }
+          
+        }
+        return cnt;
+    }
+}
